f10949e01fb3af67469334b108f95d21cf60f352
divby0exc <seddigh.daniel@gmail.com>
2023-06-06 19:40:36 +0200

    added header, button to right and main titles etc

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
   return (
     <div>
-    <div className='container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10'>
-        <div className='tr'>
-        <button className="py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">Sign in</button>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <div className="tr">
+            <span>Browsing as guest...</span><br />
+          <button className="py-2 px-4 bg-blue-600 hover:bg-blue-700 rounded-md text-white text-sm">
+            Sign in
+          </button>
         </div>
-        <div className='tc'>
-        <h1 className="col-start-1 row-start-2 mt-4 max-w text-4xl font-extrabold tracking-tight text-slate-900 sm:text-7xl xl:max-w">Library</h1>
+        <div className="tc">
+          <h1 className="col-start-1 row-start-2 mt-4 max-w text-4xl font-extrabold tracking-tight text-slate-900 sm:text-7xl xl:max-w">
+            Library
+          </h1>
         </div>
-        
-    </div>
-    <div className=''>
-            <input type="text" placeholder='Search query...' className='rounded-xl shadow border p-4'/>
+      </div>
+      <div className="">
+        <input
+          type="text"
+          placeholder="Search query..."
+          className="rounded-xl shadow border p-4"
+        />
+      </div>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 flex flex-row flex-wrap">
+        <div className="border bg-gray-200 text-4xl font-extrabold p-7 m-4">
+          Book title
         </div>
+        <div className="border bg-gray-200 text-4xl font-extrabold p-7 m-4">
+          Book author
+        </div>
+        <div className="border bg-gray-200 text-4xl font-extrabold p-7 m-4">
+          Availability
+        </div>
+      </div>
     </div>
   );
 };
