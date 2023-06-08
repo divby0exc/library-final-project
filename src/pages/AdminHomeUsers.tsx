9b11eed92b22f5255137d44aeccfecf37c8d0093
divby0exc <seddigh.daniel@gmail.com>
2023-06-09 00:36:36 +0200

    added files for users view as admin

--- src/pages/AdminHomeUsers.tsx
+++ src/pages/AdminHomeUsers.tsx
@@ -21,7 +21,6 @@ const AdminHome = () => {
       </div>
       <div className="container mx-auto flex justify-around gap-2">
         <SearchUsername />
-        <AddButton />
       </div>
       <div className="container mx-auto flex justify-end gap-2 mt-2">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
