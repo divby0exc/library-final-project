2b81778f4a9277ddd0f2ce1336eb69b6114cab47
divby0exc <seddigh.daniel@gmail.com>
2023-06-11 03:41:54 +0200

    starting with the jwt auth

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import 'tachyons'
-import GuestHome from './pages/GuestHome';
-import UserHome from './pages/UserHome';
-import AdminHome from './pages/AdminHomeBooks';
-
-import Login from './pages/Login';
-import Register from './pages/Register';
-
-import AdminHomeUsers from './pages/AdminHomeUsers';
-import EditModelForm from './components/AdminComponents/EditModelForm';
-import AdminHomeBooks from './pages/AdminHomeBooks';
 
 
 const root = ReactDOM.createRoot(
@@ -20,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <AdminHomeUsers />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
