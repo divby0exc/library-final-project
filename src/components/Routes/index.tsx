ea610a7c312d15c061fa32e75a4cec5269eefb70
divby0exc <seddigh.daniel@gmail.com>
2023-06-12 16:42:49 +0200

    organizing routes in a route foldernow. created protectedroute comp and moved index to same folder

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from '../../App';
 
-import Login from './pages/Login';
+import Login from '../../pages/Login';
 
 
 const root = ReactDOM.createRoot(
